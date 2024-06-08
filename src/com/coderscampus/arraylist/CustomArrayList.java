a499fc4d520d162f9ed8f36d11f87929b46dba36
nayelibueso <nayelibueso50@gmail.com>
2024-06-07 23:20:37 -0500

    Adding to boolean add method

--- src/com/coderscampus/arraylist/CustomArrayList.java
+++ src/com/coderscampus/arraylist/CustomArrayList.java
@@ -2,11 +2,19 @@ package com.coderscampus.arraylist;
 
 public class CustomArrayList<T> implements CustomList<T> {
 	Object[] items = new Object[10];
-
+	private Integer size = 0;
 	@Override
 	public boolean add(T item) {
+		if (size == items.length) {
+			resizeArray();
+		}
+		items [size++] = item;
+		return true;
+	}
+
+	private void resizeArray() {
 		// TODO Auto-generated method stub
-		return false;
+		
 	}
 
 	@Override
